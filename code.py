ef7a446ac37b555af032120a3e433acc363471c3
unknown <hamzajw64@gmail.com>
2023-11-25 11:31:55 +0100

    =Coose file button and initial state fixed

--- code.py
+++ code.py
@@ -2,6 +2,7 @@
 # frame = [1699348572.49173, 2, '0x23A', 'Rx', 'd', 8, [0, 0, 4, 0, 0, 0, 0, 0]]
 from Decoder import *
 from datetime import datetime
+# import decoder_gui
 
 car_variables = {
 	'ID': '0x000',
@@ -25,40 +26,42 @@ get_time = datetime.fromtimestamp
 
 updated_frames = []
 all_frames = []
-trace = get_trace('hamza.txt')
 Ids = {'car_status': '0x2FC', 'key_button_status': '0x23A', 'contact_status':'0x2CA', 'key_info': '0x723'}
 
-for frame in trace:
-	if frame[2] == Ids.get('car_status'):
-		car_status = get_car_status(frame)
-		if car_status != car_variables.get('car_status'):
-			time = get_time(frame[0]).strftime("%I%p %M:%S")
-			car_variables.update({'car_status':car_status,'time':time,'ID':frame[2],'frame':frame[6],'txrx':frame[3]})
-			updated_frames.append(car_variables.copy())
-
-	elif frame[2] == Ids.get('key_button_status'):
-		key_button_status = get_key_button_status(frame)
-		for kb_status in key_button_status:
-			if key_button_status.get(kb_status) != car_variables.get(kb_status):
-				time = time = get_time(frame[0]).strftime("%I%p %M:%S")
-				car_variables.update({kb_status:key_button_status.get(kb_status),'time':time,'ID':frame[2],'frame':frame[6],'txrx':frame[3]})
+def trace_construct(file):
+	trace = get_trace(file)
+	for frame in trace:
+		if frame[2] == Ids.get('car_status'):
+			car_status = get_car_status(frame)
+			if car_status != car_variables.get('car_status'):
+				time = get_time(frame[0]).strftime("%I%p %M:%S")
+				car_variables.update({'car_status':car_status,'time':time,'ID':frame[2],'frame':frame[6],'txrx':frame[3]})
 				updated_frames.append(car_variables.copy())
 
-	elif frame[2] == Ids.get('contact_status'):
-		contact_status = get_contact_status(frame)
-		for status in contact_status:
-			if contact_status.get(status) != car_variables.get(status):
-				time = time = get_time(frame[0]).strftime("%I%p %M:%S")
-				car_variables.update({status:contact_status.get(status),'time':time,'ID':frame[2],'frame':frame[6],'txrx':frame[3]})
-				updated_frames.append(car_variables.copy())
+		elif frame[2] == Ids.get('key_button_status'):
+			key_button_status = get_key_button_status(frame)
+			for kb_status in key_button_status:
+				if key_button_status.get(kb_status) != car_variables.get(kb_status):
+					time = time = get_time(frame[0]).strftime("%I%p %M:%S")
+					car_variables.update({kb_status:key_button_status.get(kb_status),'time':time,'ID':frame[2],'frame':frame[6],'txrx':frame[3]})
+					updated_frames.append(car_variables.copy())
 
-	elif frame[2] == Ids.get('key_info'):
-		key_position = get_key_info(frame)[0]
-		if key_position != car_variables.get('key_position'):
-			time = time = get_time(frame[0]).strftime("%I%p %M:%S")
-			car_variables.update({'key_position': key_position,'time':time,'ID':frame[2],'frame':frame[6],'txrx':frame[3]})
-			updated_frames.append(car_variables)
-	all_frames.append(car_variables.copy())
+		elif frame[2] == Ids.get('contact_status'):
+			contact_status = get_contact_status(frame)
+			for status in contact_status:
+				if contact_status.get(status) != car_variables.get(status):
+					time = time = get_time(frame[0]).strftime("%I%p %M:%S")
+					car_variables.update({status:contact_status.get(status),'time':time,'ID':frame[2],'frame':frame[6],'txrx':frame[3]})
+					updated_frames.append(car_variables.copy())
+
+		elif frame[2] == Ids.get('key_info'):
+			key_position = get_key_info(frame)[0]
+			if key_position != car_variables.get('key_position'):
+				time = time = get_time(frame[0]).strftime("%I%p %M:%S")
+				car_variables.update({'key_position': key_position,'time':time,'ID':frame[2],'frame':frame[6],'txrx':frame[3]})
+				updated_frames.append(car_variables)
+		all_frames.append(car_variables.copy())
+	return updated_frames
 
 
 
--- decoder_gui.py
+++ decoder_gui.py
@@ -1,60 +1,12 @@
 import tkinter as tk
 # from tkinter import Tk     # from tkinter import Tk for Python 3.x
-import code, Decoder
+# import code, Decoder
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
 
 
 
 # frame = [1699348572.49173, 2, '0x23A', 'Rx', 'd', 8, [0, 0, 4, 0, 0, 0, 0, 0]]
-my_list = [{
-	'ID':'0x000',
-	'txrx': None,
-	'frame':'1, 0, 0, 0, 0, 0, 0, 0',
-	'time' : '10:56',
-	'car_status': 'Unlocked',
-	'key_position': 'Inside',
-
-	'drd_status_cont': 'Closed', 
-	'psd_status_cont':'Closed', 
-	'drdr_status_cont':'Opened', 
-	'psdr_status_cont':'Closed',
-
-	'unlock_button_status':'Hard pressed',
-	'lock_button_status':'Soft pressed',
-	'_3d_button_status': 'Hard Pressed'
-},
-{	'ID':'0x000',
-	'frame':'2, 0, 0, 0, 0, 0, 0, 0',
-	'time' : '10:57',
-	'car_status': 'Locked',
-	'key_position': 'Outside',
-
-	'drd_status_cont': 'half opened', 
-	'psd_status_cont':'Opened', 
-	'drdr_status_cont':'Closed', 
-	'psdr_status_cont':'Half Opened',
-
-	'unlock_button_status':'Not pressed',
-	'lock_button_status':'Not pressed',
-	'_3d_button_status': 'Not Pressed'
-},
-{	
-	'ID':'0x000',
-	'frame':'3, 0, 0, 0, 0, 0, 0, 0',
-	'time' : '10:58',
-	'car_status': 'Secured',
-	'key_position': 'Unkown',
-
-	'drd_status_cont': 'Opened', 
-	'psd_status_cont':'Closed', 
-	'drdr_status_cont':'Half opened', 
-	'psdr_status_cont':'Opened',
-
-	'unlock_button_status':'Pressed',
-	'lock_button_status':'Pressed',
-	'_3d_button_status': 'pressed'
-}]
 
 
 
@@ -72,13 +24,19 @@ root.geometry("950x400")
 root.title('Can Decoder by Hamza')
 
 
-
-my_list = code.updated_frames
-
+index = -1
+my_list = []
 def choose_file():
+	global my_list
+	from code import trace_construct   # I import it here to avoid circular import (when two modules import each other)
 	filename = askopenfilename()
+	my_list = trace_construct(filename)
+	frame_up()
+
 	
 def frame_up():
+	if not(my_list):
+		return
 	global index
 	if index >= len(my_list)-1:
 		index =-1
@@ -129,6 +87,8 @@ def frame_up():
 
 
 def frame_down():
+	if not(my_list):
+		return
 	global index
 	if index <=0:
 		index = len(my_list)
@@ -182,9 +142,6 @@ def frame_down():
 
 
 
-index = 0
-
-
 previous_frame_button = tk.Button(root,text ="<<",command=frame_down)
 previous_frame_button.place(x=10,y=10,width=42)
 
@@ -192,24 +149,24 @@ next_frame_button = tk.Button(root,text =">>",command=frame_up)
 next_frame_button.place(x=55,y=10,width=42)
 
 frame_text = tk.StringVar()
-frame_text.set(my_list[0]['frame'])
+# frame_text.set(my_list[0]['frame'])
 frame_label = tk.Label(root,textvariable=frame_text,width=20,bg='white')
 frame_label.place(x=110,y=10)
 
 index_value = tk.StringVar()
-index_value.set(f'#{index+1} :')
+# index_value.set(f'#{index+1} :')
 index_label = tk.Label(root,textvariable=index_value,bg='white')
 index_label.place(x=110,y=10)
 
 id_btn = tk.Button(root,text='ID:',width=4)
 id_btn.place(x=265,y=10)
 id_value = tk.StringVar()
-id_value.set(my_list[0]['ID'])
+# id_value.set(my_list[0]['ID'])
 id_label = tk.Label(root,textvariable=id_value,width=10,bg='white')
 id_label.place(x=310,y=10)
 
 TxRx_value = tk.StringVar()
-TxRx_value.set(my_list[0]['txrx'])
+# TxRx_value.set(my_list[0]['txrx'])
 TxRx_label = tk.Label(root,textvariable=TxRx_value,width=2,bg='white')
 TxRx_label.place(x=390,y=10)
 
@@ -217,7 +174,7 @@ TxRx_label.place(x=390,y=10)
 time_btn = tk.Button(root,text='Time : ')
 time_btn.place(x=10,y=40,width=90)
 time_value = tk.StringVar()
-time_value.set(my_list[0]['time'])
+# time_value.set(my_list[0]['time'])
 time_label = tk.Label(root,textvariable=time_value,width=15,bg='white')
 time_label.place(x=110,y=40)
 
@@ -225,7 +182,7 @@ time_label.place(x=110,y=40)
 car_status_btn = tk.Button(root,text='Car Status: ')
 car_status_btn.place(x=10,y=70,width=90)
 car_status_value = tk.StringVar()
-car_status_value.set(my_list[0]['car_status'])
+# car_status_value.set(my_list[0]['car_status'])
 car_status_label = tk.Label(root,textvariable=car_status_value,width=15,bg='white')
 car_status_label.place(x=110,y=70)
 
@@ -237,35 +194,35 @@ status_color_btn.place(x=200,y=70,height=20)
 key_pos_btn = tk.Button(root,text='Key Position : ')
 key_pos_btn.place(x=10,y=100,width=90)
 key_pos_value = tk.StringVar()
-key_pos_value.set(my_list[0]['key_position'])
+# key_pos_value.set(my_list[0]['key_position'])
 key_pos_label = tk.Label(root,textvariable=key_pos_value,width=15,bg='white')
 key_pos_label.place(x=110,y=100)
 
 unlock_btn_btn = tk.Button(root,text='Unlock button : ')
 unlock_btn_btn.place(x=10,y=130,width=90)
 unlock_btn_value = tk.StringVar()
-unlock_btn_value.set(my_list[0]['unlock_button_status'])
+# unlock_btn_value.set(my_list[0]['unlock_button_status'])
 unlock_btn_label = tk.Label(root,textvariable=unlock_btn_value,width=15,bg='white')
 unlock_btn_label.place(x=110,y=130)
 
 lock_btn_btn = tk.Button(root,text='Lock button : ')
 lock_btn_btn.place(x=10,y=160,width=90)
 lock_btn_value = tk.StringVar()
-lock_btn_value.set(my_list[0]['lock_button_status'])
+# lock_btn_value.set(my_list[0]['lock_button_status'])
 lock_btn_label = tk.Label(root,textvariable=lock_btn_value,width=15,bg='white')
 lock_btn_label.place(x=110,y=160)
 
 _3d_btn_btn = tk.Button(root,text='3d button : ')
 _3d_btn_btn.place(x=10,y=190,width=90)
 _3d_btn_value = tk.StringVar()
-_3d_btn_value.set(my_list[0]['_3d_button_status'])
+# _3d_btn_value.set(my_list[0]['_3d_button_status'])
 _3d_btn_label = tk.Label(root,textvariable=_3d_btn_value,width=15,bg='white')
 _3d_btn_label.place(x=110,y=190)
 
 drd_cont_btn = tk.Button(root,text='DRD status: ')
 drd_cont_btn.place(x=260,y=70,width=100)
 drd_cont_value = tk.StringVar()
-drd_cont_value.set(my_list[0]['drd_status_cont'])
+# drd_cont_value.set(my_list[0]['drd_status_cont'])
 drd_cont_label = tk.Label(root,textvariable=drd_cont_value,width=15,bg='white')
 drd_cont_label.place(x=370,y=70)
 
@@ -273,7 +230,7 @@ drd_cont_label.place(x=370,y=70)
 drdr_cont_btn = tk.Button(root,text='DRDR status: ')
 drdr_cont_btn.place(x=260,y=100,width=100)
 drdr_cont_value = tk.StringVar()
-drdr_cont_value.set(my_list[0]['drdr_status_cont'])
+# drdr_cont_value.set(my_list[0]['drdr_status_cont'])
 drdr_cont_label = tk.Label(root,textvariable=drdr_cont_value,width=15,bg='white')
 drdr_cont_label.place(x=370,y=100)
 
@@ -281,7 +238,7 @@ drdr_cont_label.place(x=370,y=100)
 psd_cont_btn = tk.Button(root,text='PSD status: ')
 psd_cont_btn.place(x=260,y=130,width=100)
 psd_cont_value = tk.StringVar()
-psd_cont_value.set(my_list[0]['psd_status_cont'])
+# psd_cont_value.set(my_list[0]['psd_status_cont'])
 psd_cont_label = tk.Label(root,textvariable=psd_cont_value,width=15,bg='white')
 psd_cont_label.place(x=370,y=130)
 
@@ -289,7 +246,7 @@ psd_cont_label.place(x=370,y=130)
 psdr_cont_btn = tk.Button(root,text='PSDR status: ')
 psdr_cont_btn.place(x=260,y=160,width=100)
 psdr_cont_value = tk.StringVar()
-psdr_cont_value.set(my_list[0]['psdr_status_cont'])
+# psdr_cont_value.set(my_list[0]['psdr_status_cont'])
 psdr_cont_label = tk.Label(root,textvariable=psdr_cont_value,width=15,bg='white')
 psdr_cont_label.place(x=370,y=160)
 
